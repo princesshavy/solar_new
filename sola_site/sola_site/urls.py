d679543ecfc0717b09201e7765a7182503c6eae2
princesshavy <havilah757@gmail.com>
2025-07-20 16:31:26 +0100

    problem

--- sola_site/sola_site/urls.py
+++ sola_site/sola_site/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from website.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage')
 ]
